bb8c91d45ebe807f33668b1c41e735e348babd33
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-09-06 18:44:23 -0300

    projeto finalizado

--- script/script.js
+++ script/script.js
@@ -1,1 +1,12 @@
 const listas = document.querySelectorAll('.lista')
+
+function ativaLink () {
+    for(let i of listas) {
+        i.classList.remove('ativo')
+    }
+    this.classList.add('ativo')
+}
+ 
+for (let i of listas) {
+    i.addEventListener('click', ativaLink)
+}
